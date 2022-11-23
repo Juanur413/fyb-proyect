c908bb49de14992f1cf1438355b1592ee183b9dc
RubenU2002 <rsantiagourrego@gmail.com>
2022-11-22 21:31:05 -0500

    Registro con validación de email y correo

--- frontend/notas-app/src/components/Register.js
+++ frontend/notas-app/src/components/Register.js
@@ -19,7 +19,7 @@ const Register = () => {
         axios.post('/api/usuario/agregarusuario',usuario)
         .then(res=>{
             //aca pueden poner a donde ir despues de un login correcto navigate()
-            res ? navigate('/registroCorrecto') : alert("Error en el registro");
+            res.data!=="" ? navigate('/registroCorrecto') : alert("Error en el registro");
         })
         .then(err=>{console.log(err)})
     }
@@ -37,12 +37,12 @@ const Register = () => {
 
                         <div className="mb-3">
                             <label>Email</label>
-                            <input className="form-control" placeholder="Ingrese su cuenta de correo electrónico" type="email" id="title" name="title" onChange={(e) => setEmail(e.target.value)}/>
+                            <input className="form-control" placeholder="Ingrese su cuenta de correo electrónico" type="email" id="title" name="title" onChange={(e) => setEmail(e.target.value)} required/>
 
                         </div>
                         <div className="mb-3">
                             <label>Contraseña</label>
-                            <input className="form-control" placeholder="Ingrese su nueva contraseña" type="password" id="author" name="author" onChange={(e) => setPassword(e.target.value)}/>
+                            <input className="form-control" placeholder="Ingrese su nueva contraseña" type="password" id="author" name="author" onChange={(e) => setPassword(e.target.value)} required/>
                         </div>
                         <button type="submit" onClick={agregarUsuario} class="btn btn-primary mb-3 ">Registrarse</button>
                         <div>
