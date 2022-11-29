dfcf2608c7c13021b611221f844410cc779f24e2
Federico Gaviria Zambrano <federico.gaviriaz@gmail.com>
2022-11-28 22:16:23 -0500

    nuevamente

--- conexion.js
+++ conexion.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://admin:admin@cluster0.igodnqb.mongodb.net/?retryWrites=true&w=majority/crudmernstack');
 
@@ -9,14 +9,3 @@ objetodb.on('error',() => { console.log("Error en la conexión a mongodb") })
 
 module.exports = mongoose
 
-=======
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://admin:admin@cluster0.igodnqb.mongodb.net/?retryWrites=true&w=majority/crudmernstack');
-
-const objetodb = mongoose.connection
-
-objetodb.on('connected',() => { console.log("conexión correcta a mongoDB") })
-objetodb.on('error',() => { console.log("Error en la conexión a mongodb") })
-
-module.exports = mongoose
->>>>>>> 4eb0dfa76ac7b0cadcb33dd0aff85cec6a908d97
